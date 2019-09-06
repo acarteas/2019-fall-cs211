cb3834701c70746be41ccc7200cf544cc6f5b723
amt954 <amt954@humboldt.edu>
2019-09-06 13:06:36 -0700

    Updated

--- projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
@@ -18,11 +18,19 @@ int main(void)
 	main_window = initscr();
 
 	//resize our window
-	resize_term(5000, 5000);
-	//getmaxyx(main_window, num_rows, num_cols);
-	//resize_term(num_rows - 1, num_cols - 1);
-	//getmaxyx(main_window, num_rows, num_cols);
+	resize_term(100, 100);
+	getmaxyx(main_window, num_rows, num_cols);
+	resize_term(num_rows - 1, num_cols - 1);
+	getmaxyx(main_window, num_rows, num_cols);
+	attron(A_DIM);
 	wborder(main_window, 0, 0, 0, 0, 0, 0, 0, 0);
+	attroff(A_DIM);
+
+	//create text field
+	WINDOW * sub_window;
+	sub_window = subwin(main_window, num_rows - 2, num_cols - 2, 1, 1);
+	//Use to verify location of sub_window
+	box(sub_window, 047, 055);
 
 	//turn off keyboard echo
 	noecho();
@@ -42,45 +50,67 @@ int main(void)
 		printf("Your terminal does not support color\n");
 		exit(1);
 	}
-	start_color();			/* Start color 			*/
-	init_pair(1, COLOR_WHITE, COLOR_BLUE); //sets the color of text and background
+	start_color(); /* Start color */
+	init_pair(1, COLOR_WHITE, COLOR_BLACK); //sets the color of text and background
 
 	attron(COLOR_PAIR(1));
+	attron(A_UNDERLINE);
 	mvaddstr(0, 2, "File");
 	mvaddstr(0, 8, "Edit");
 	mvaddstr(0, 14, "Options");
 	mvaddstr(0, 23, "Tools");
 	mvaddstr(0, 30, "About");
+	attroff(A_UNDERLINE);
 	attroff(COLOR_PAIR(1));
 
 
 	//add border to screen
-	for (int i = 0; i < num_cols; i++)
+	/*for (int i = 0; i < num_cols; i++)
 	{
-		//top row
-		mvaddch(0, i, ACS_BLOCK);
+	//top row
+	mvaddch(0, i, ACS_BLOCK);
 
-		//bottom row
-		mvaddch(num_rows - 1, i, ACS_BLOCK);
+	//bottom row
+	mvaddch(num_rows - 1, i, ACS_BLOCK);
 	}
 
 	for (int i = 0; i < num_cols; i++)
 	{
-		//left column
-		mvaddch(i, 0, ACS_BLOCK);
+	//left column
+	mvaddch(i, 0, ACS_BLOCK);
 
-		//right column
-		mvaddch(i, num_cols - 1, ACS_BLOCK);
-	}
+	//right column
+	mvaddch(i, num_cols - 1, ACS_BLOCK);
+	}*/
 
 	//refresh tells curses to draw everything
 	refresh();
+	wrefresh(sub_window);
 
 	//END OF PROGRAM LOGIC GOES HERE
 
 	//Pause for user input
-	char input = getch();
+	//char input = getch();
+	attron(A_STANDOUT);
+	mvwaddstr(sub_window, num_cols / 2, num_rows / 2, "TYPE ASTERICK * TO EXIT");
+	attroff(A_STANDOUT);
+	wmove(sub_window, 4, 1);
+	char typing = ' ';
+
+	while (typing != '*')
+	{
+		int type_input = getch();
+		mvwaddch(sub_window, num_cols / 2, num_rows / 2, type_input);
+		//addch(type_input);
+		typing = type_input;
+	}
+
+	if (typing == '*')
+	{
+		return 0;
+	}
 
 	//end curses mode
+	delwin(sub_window);
 	endwin();
 }
