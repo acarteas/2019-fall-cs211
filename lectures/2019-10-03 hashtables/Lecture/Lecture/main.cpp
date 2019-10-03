38bc0f947263ea4b1a80dab05d35b7995eeee9b9
Adam Carter <mail@adam-carter.com>
2019-10-02 21:12:21 -0700

    added next two lectures to the repository.

---
+++ lectures/2019-10-03 hashtables/Lecture/Lecture/main.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <list> //Doubly-linked list
+#include <stack>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+//Task #1: Count # of words in a string
+
+//Task #2: Calculate average rainfall
+
+int main(int argc, char* argv[])
+{
+	
+}
