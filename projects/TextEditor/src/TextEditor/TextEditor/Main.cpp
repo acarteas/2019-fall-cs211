09c47c915fba6bfb11d486a84dd66d61fce0598d
James Schulz <js599@humboldt.edu>
2019-10-03 22:51:22 -0700

    Cleaned up unnecessary code

--- projects/TextEditor/src/TextEditor/TextEditor/Main.cpp
+++ projects/TextEditor/src/TextEditor/TextEditor/Main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void moveCursor(WINDOW* win, int y, int x); // Moves cursor based on arrow key input
+
 void drawText(WINDOW* win, vector<char> &vect, const int term_rows, const int term_cols, int start); // Displays  contents of vector in main window and get length
 void get_curs_pos(WINDOW* get, WINDOW* write, int rows); // Writes cursor's y and x position
 
@@ -117,166 +117,135 @@ int main(int argc, char* argv[]) {
 
 
 
-	//int file_length;
-
-	/*mvwprintw(botWin, 1, 1, "File length:");
-	mvwprintw(botWin, 1, 13, "%d", file_length);
-	wrefresh(botWin);
-*/
-
-	
-
-	
-
-
-	//int curs_pos = 0;
-	//int y_pos = 0;
-
-	//int key;
-
-	//while (1) {
-	//	key = wgetch(mWin);
-	//	switch (key) {
-	//	
-	//	case KEY_RIGHT:
-	//		if (curs_pos % 116 == 0) {
-	//			y_pos++;
-	//		}
-	//		moveCursor(mWin, y_pos, curs_pos);
-	//		curs_pos++;
-	//	}
-
-	//}
 	
 	
 	int start = 0;
-	int fyle_rows = fyle.size() / 114;
-	int row_num = 1;
+	int fyle_rows = fyle.size() / 114; // Fle rows
+	int row_num = 1; // Starting row num
+
 	mvwprintw(botWin, 1, 16, "%d", fyle_rows);
 	mvwprintw(botWin, 1, 1, "Number of rows:");
-	wrefresh(mWin);
 	get_curs_pos(mWin, botWin, row_num);
 	
-	drawText(mWin, fyle, term_rows, term_cols, start);
+	drawText(mWin, fyle, term_rows, term_cols, start); // Draws text to screen
+
+	int vect_pos = 0; // Position of cursor in vector
+	int key_x_pos = 0; // Position of writing
+	int key_y_pos = 0; // Position of writing
+	
 	// Cursor position and key grab
 	int curs_x_pos = 0;
 	int curs_y_pos = 0;
-	//moveCursor(mWin, curs_y_pos, curs_x_pos);
-	wmove(mWin, curs_y_pos, curs_x_pos);
+	
 	int key;
 	key = wgetch(mWin);
-	// Movement of cursor with arrow keys
+	
 	while (key != KEY_F(8)) {
+
+		// Experimental
+
+		mvwprintw(botWin, 1, 47, "%d", vect_pos);
+		wrefresh(botWin);
+
+
+
+
+
+
+
+
+		wmove(mWin, curs_y_pos, curs_x_pos); // Write new position of cursor
+		get_curs_pos(mWin, botWin, row_num); // Write coordinates of cursor in bottom window
+		wrefresh(mWin); // Refreshes main window
+		
 		key = wgetch(mWin);
+
+		// If key is valid input
+		if (key >= 32
+			&& key <= 126 
+			|| key == 10
+			|| key == 8) {
+			
+			// Checks if enter has been pressed and starts new line
+			if (key == 10) {
+				curs_y_pos += 1;
+				curs_x_pos = 0;
+				vect_pos += 115;
+				continue;
+			}
+
+				// Checks if backspace was entered
+				if (key == 8) {
+					if (curs_x_pos == 0 && curs_y_pos > 0) {
+						curs_y_pos--;
+						curs_x_pos = term_cols - 4;
+					}
+						if (curs_x_pos > 0) {
+							curs_x_pos--;
+							mvwaddch(mWin, curs_y_pos, curs_x_pos, 32);
+						}
+					continue;
+				}
+			
+		// Prints typed char and updates for next position
+		mvwprintw(mWin, curs_y_pos, curs_x_pos, "%c", key);
+		curs_x_pos++;
+		vect_pos++;
+		}
+		
+		// Movement of cursor with arrow keys
 		switch (key) {
 		case KEY_UP:
 			if (curs_y_pos == 0 && start > 0) {
 				start -= term_cols - 4;
 				drawText(mWin, fyle, term_rows, term_cols, start);
 				row_num--;
-				get_curs_pos(mWin, botWin, row_num);
-				wrefresh(mWin);
 			}
 			if (curs_y_pos > 0) {
 				curs_y_pos--;
+				vect_pos -= 116;
 				row_num--;
-				//moveCursor(mWin, curs_y_pos, curs_x_pos);
-				wmove(mWin, curs_y_pos, curs_x_pos);
-				get_curs_pos(mWin, botWin, row_num);
-				wrefresh(mWin);
 			}
 			break;
 		case KEY_DOWN:
 			if (curs_y_pos == term_rows - 7 && row_num < fyle_rows) {
 				start += term_cols - 4;
+				vect_pos += 116;
+				curs_y_pos = term_rows - 7;
 				drawText(mWin, fyle, term_rows, term_cols, start);
 				row_num++;
-				get_curs_pos(mWin, botWin, row_num);
-				wrefresh(mWin);
 			}
 			if (curs_y_pos < term_rows - 7) {
-
 				curs_y_pos++;
+				vect_pos += 116;
 				row_num++;
-				//moveCursor(mWin, curs_y_pos, curs_x_pos);
-				wmove(mWin, curs_y_pos, curs_x_pos);
-				
-				get_curs_pos(mWin, botWin, row_num);
-				wrefresh(mWin);
 			}
 			break;
 		case KEY_RIGHT:
 			if (curs_x_pos < term_cols - 4) {
 				curs_x_pos++;
-				//moveCursor(mWin, curs_y_pos, curs_x_pos);
-				wmove(mWin, curs_y_pos, curs_x_pos);
-				wrefresh(mWin);
-
-				get_curs_pos(mWin, botWin, row_num);
+				vect_pos++;
 			}
 			break;
 		case KEY_LEFT:
 			if (curs_x_pos > 0) {
 				curs_x_pos--;
-				//moveCursor(mWin, curs_y_pos, curs_x_pos);
-				wmove(mWin, curs_y_pos, curs_x_pos);
-				wrefresh(mWin);
-
-				get_curs_pos(mWin, botWin, row_num);
+				vect_pos--;
 			}
 			break;
 		
 		default:
 			break;
 		}
-
+		
 	}
-	
-
-	//
-	//int key_x_pos = 0; // Position of writing
-	//int key_y_pos = 0; // Position of writing
-	//
-	//int ch;
-	//ch = wgetch(mWin);
-
-
-
-	//// Writes user input to screen
-	//while (ch != KEY_F(8)) {
-	//	
-	//	// Checks if enter has been pressed and starts new line
-	//	if (ch == 10) {
-	//		key_y_pos += 1;
-	//		key_x_pos = 0;
-	//	
-	//	}
-
-	//	// Checks if line has reached edge of terminal and starts a new line
-	//	if (key_x_pos == term_cols - 3) {
-	//		key_y_pos += 1;
-	//		key_x_pos = 1;
-	//	}
-
-	//	// Prints typed char and updates for next position
-	//	//mvwprintw(mWin, key_y_pos, key_x_pos, "%c", ch);
-	//	waddch(mWin, ch);
-	//	key_x_pos += 1;
-	//	wrefresh(mWin);
-	//	
-	//	ch = wgetch(mWin);
-	//}
-	
-
 	endwin();
 
 return 0;
 
 }
 
-void moveCursor(WINDOW* win, int y, int x) {
-	wmove(win, y, x);
-}
+
 
 void drawText(WINDOW* win, vector<char>& vect, const int term_rows, const int term_cols, int start) {
 	
@@ -284,7 +253,6 @@ void drawText(WINDOW* win, vector<char>& vect, const int term_rows, const int te
 	int y = 0;
 	int file_pos = start;
 	int file_end = (term_cols - 4) * (term_rows - 6) + file_pos - 2;
-
 
 	if (file_pos < vect.size()) {
 
@@ -296,12 +264,14 @@ void drawText(WINDOW* win, vector<char>& vect, const int term_rows, const int te
 				x = 0;
 			}
 
+			// If all is normal print char and advance 1 position
 			if (file_pos < vect.size() && file_pos >= 0) {
 				mvwaddch(win, y, x, vect[file_pos]);
 				x++;
 			
 			}
 			
+			// Print blank char
 			else {
 				mvwaddch(win, y, x, 32);
 				x++;
@@ -319,18 +289,22 @@ void get_curs_pos(WINDOW* get, WINDOW* write, int rows) {
 	int cursor_y_pos;
 	getyx(get, cursor_y_pos, cursor_x_pos);
 
-	mvwprintw(write, 1, 22, "X:");
+	// X position
+	mvwprintw(write, 1, 22, "X:"); 
 	mvwprintw(write, 1, 24, "%d", cursor_x_pos);
 
+	// Y position
 	mvwprintw(write, 1, 27, "Y:");
 	mvwprintw(write, 1, 29, "%d", cursor_y_pos);
 
+	// Row number
 	mvwprintw(write, 1, 32, "Row:");
 	mvwprintw(write, 1, 36, "%d", rows);
 	
 
 	wrefresh(write);
 
+	//Blank spaces
 	mvwaddch(write, 1, 25, 32);
 	mvwaddch(write, 1, 26, 32);
 	mvwaddch(write, 1, 30, 32);
