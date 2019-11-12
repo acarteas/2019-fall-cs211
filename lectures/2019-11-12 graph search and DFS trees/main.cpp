e6c85fd325bb41380652f44e323af693139dbe56
Adam Carter <mail@adam-carter.com>
2019-11-12 10:19:59 -0800

    final checkin of today's lecture.

--- lectures/2019-11-12 graph search and DFS trees/main.cpp
+++ lectures/2019-11-12 graph search and DFS trees/main.cpp
@@ -1,11 +1,93 @@
 #include <iostream>
 #include <string>
+#include <queue>
 #include "sorting\Sorting.h"
 #include "benchmarks\SortingBenchmark.h"
 #include "ADTs\Vector.h"
 #include "ADTs\StringGraphNode.h"
 
 using namespace std;
+
+bool isConnectedHelper(
+	StringGraphNode* node, 
+	string destination,
+	unordered_map<string, int> visited_map
+	)
+{
+	if (node == nullptr)
+	{
+		//null node means not connected, so return false
+		return false;
+	}
+
+	if (node->getKey() == destination)
+	{
+		return true;
+	}
+
+	//mark as visited
+	visited_map[node->getKey()]++;
+
+	//if we've gotten this far, we need to visit all of our
+	//children
+	for (auto node : node->getEdges())
+	{
+		//check node if not seen before
+		if (visited_map.find(node->getKey()) == visited_map.end())
+		{
+			bool found = isConnectedHelper(
+				dynamic_cast<StringGraphNode*>(node),
+				destination,
+				visited_map
+			);
+			if (found == true)
+			{
+				return true;
+			}
+		}
+		else
+		{
+			//back edge
+		}
+	}
+	return false;
+}
+
+bool isConnected(StringGraphNode* start, string destination)
+{
+	unordered_map<string, int> found_map{};
+	return isConnectedHelper(start, destination, found_map);
+}
+
+bool isConnectedBfs(StringGraphNode* start, string destination)
+{
+	queue<StringGraphNode*> to_visit{};
+	unordered_map<string, int> visited{};
+	to_visit.push(start);
+	while (to_visit.empty() == false)
+	{
+
+		//grab next node
+		StringGraphNode* next = to_visit.front();
+		to_visit.pop();
+
+		//if not seen, process
+		if (visited[next->getKey()] == 0)
+		{
+			visited[next->getKey()]++;
+
+			for (auto node : next->getEdges())
+			{
+				if (node->getKey() == destination)
+				{
+					return true;
+				}
+				to_visit.push(dynamic_cast<StringGraphNode*>(node));
+			}
+		}
+	}
+	return false;
+}
 
 int main(void)
 {
@@ -24,10 +106,16 @@ int main(void)
     graph["A"]->addEdge(graph["B"]);
     graph["A"]->addEdge(graph["F"]);
 
-    //B is connected to A, C, and E
+    //B is connected to A, C, and F
     graph["B"]->addEdge(graph["A"]);
     graph["B"]->addEdge(graph["C"]);
     graph["B"]->addEdge(graph["F"]);
+
+	//connect E to F and F to E
+	graph["E"]->addEdge(graph["F"]);
+	graph["F"]->addEdge(graph["E"]);
+
+	cout << isConnectedBfs(graph["A"], "E") << endl;
 
     return 0;
 }
